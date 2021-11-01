ce86d594bb34f488635b359fd2dceca7ec7bcc6d
Andrew Pellum <apellum19@gmail.com>
2021-11-01 10:41:24 -0600

    Styled Errors

--- src/components/static/Errors.js
+++ src/components/static/Errors.js
@@ -1,31 +1,11 @@
-// import React from 'react'
-// import { useSelector } from 'react-redux';
-
-// const Errors = () => {
-//     const errors = useSelector(state => state.errors);
-//     console.log(errors)
-
-//     const errorMessages = errors.map((error, index) => {
-//     <h4 key={index}>{ error }</h4>
-//     console.log(error)
-//     }
-//     )
-//     return (
-//         <div>
-//             { errorMessages }
-//         </div>
-//     )
-// }
-
-// export default Errors
-
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Alert } from '@mui/material'
 
 const Errors = () => {
   const errors = useSelector(state => state.errors);
 
-  const errorMessages = errors.map((error, index) => <h4 key={index}>{error}</h4>)
+  const errorMessages = errors.map((error, index) => <Alert severity="error"  variant="filled" key={index}>{error}</Alert>)
 
   return (
     <div>
